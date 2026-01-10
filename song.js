4c251aa205d82ebe9d7eeeb075b9d2db42655a61
cloan237 <doanthibichle0407@gmail.com>
2026-01-10 09:50:00 -0800

    Add files via upload

---
+++ song.js
@@ -0,0 +1,55 @@
+const songs = [
+  {
+    "id": 1,
+    "title": "Dạ cổ hoài lang",
+    "audio": "music/daco.mp3",
+    "lyrics": [
+      { "time": 42, "text": "Từ là từ phu tướng" },
+      { "time": 46, "text": "Báu kiếm sắc phong lên đàng" },
+      { "time": 52, "text": "Vào ra luống trông tin chàng" },
+      { "time": 58, "text": "Đêm năm canh mơ màng" },
+      { "time": 66, "text": "Em luống trông tin chàng" },
+      { "time": 70, "text": "Cho gan vàng quặn đau" },
+      { "time": 78, "text": "Đường dù xa ong bướm" },
+      { "time": 83, "text": "Xin đó đừng phụ nghĩa tào khang" },
+      { "time": 90, "text": "Còn đêm luống trông tin nhạn" },
+      { "time": 94, "text": "Ngày mỏi mòn như đá vọng phu" },
+      { "time": 99, "text": "Vọng phu luống trông tin chàng" },
+      { "time": 106, "text": "Lòng xin chớ phũ phàng" },
+      { "time": 116, "text": "Chàng hỡi chàng có hay" },
+      { "time": 122, "text": "Đêm thiếp nằm luống những sầu tây" },
+      { "time": 127, "text": "Biết bao thuở đó đây sum vầy" },
+      { "time": 133, "text": "Duyên sắt cầm đừng lạt pha" },
+      { "time": 146, "text": "Thiếp nguyện cho chàng" },
+      { "time": 151, "text": "Nguyện cho chàng đặng chữ bình an" },
+      { "time": 159, "text": "Mau trở lại gia đàng" },
+      { "time": 163, "text": "Cho én nhạn hiệp đôi" },
+      { "time": 170, "text": "(Nhạc dạo giữa bài)" },
+      { "time": 203, "text": "Đường dù xa ong bướm" },
+      { "time": 209, "text": "Xin đó đừng phụ nghĩa tào khang" },
+      { "time": 215, "text": "Còn đêm luống trông tin nhạn" },
+      { "time": 220, "text": "Ngày mỏi mòn như đá vọng phu" },
+      { "time": 226, "text": "Vọng phu luống trông tin chàng" },
+      { "time": 232, "text": "Lòng xin chớ phũ phàng" },
+      { "time": 240, "text": "Chàng hỡi chàng có hay" },
+      { "time": 245, "text": "Đêm thiếp nằm luống những sầu tây" },
+      { "time": 250, "text": "Biết bao thuở đó đây sum vầy" },
+      { "time": 256, "text": "Duyên sắt cầm đừng lạt pha" },
+      { "time": 269, "text": "Thiếp nguyện cho chàng" },
+      { "time": 275, "text": "Nguyện cho chàng đặng chữ bình an" },
+      { "time": 282, "text": "Mau trở lại gia đàng" },
+      { "time": 286, "text": "Cho én nhạn hiệp đôi" },
+      { "time": 294, "text": "Mau trở lại gia đàng" },
+      { "time": 298, "text": "Cho én nhạn... hiệp đôi" }
+    ]
+  },
+  {
+    "id": 2,
+    "title": "Lan và Điệp",
+    "audio": "music/lan-diep.mp3",
+    "lyrics": [
+      { "time": 0, "text": "Lan ơi..." },
+      { "time": 7, "text": "Thôi thôi duyên phận..." }
+    ]
+  }
+];
